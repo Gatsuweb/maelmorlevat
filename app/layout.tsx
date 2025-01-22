08ae3d028cab154b56561f441df37357c3837ef9
DURAN Ivan <ivan.duran@outlook.fr>
2025-01-22 13:25:08 +0100

    correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
         {children}
         <Footer />
